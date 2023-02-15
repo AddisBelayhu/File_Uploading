1a619cdac211dc7c4af785c0560af86035e4e940
Addis Belayhun <107222638+AddisBelayhu@users.noreply.github.com>
2023-02-15 11:44:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ const btn = document.querySelector('button');
 
 btn.addEventListener('click', () =>{
 
+    //get and prepair files
     const files = []; 
     document.querySelectorAll('input[type="file"]').forEach((input) =>{
         files.push(input.files[0]);
     })
+    
+    //console.log(String(files));
+    const fd = new FormData();
+    files.forEach((file, i) => {
+    fd.append('file'+ i, file);
+})
     
 })
 
