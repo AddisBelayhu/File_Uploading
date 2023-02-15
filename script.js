a7ebfd19cdf9f6c260d1672713a8abbeb77b06bf
Addis Belayhun <107222638+AddisBelayhu@users.noreply.github.com>
2023-02-15 11:34:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const btn = document.querySelector('button');
 
 btn.addEventListener('click', () =>{
 
+    const files = []; 
+    document.querySelectorAll('input[type="file"]').forEach((input) =>{
+        files.push(input.files[0]);
+    })
     
 })
 
